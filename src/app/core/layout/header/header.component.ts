21e9e739f11106258b5ec9f1a68f4ea54a06463e
Henrique Malvar <henriquemalvar@gmail.com>
2024-03-08 14:19:09 -0300

    Refactor header component to display search section only when user is logged in

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { User } from '@interfaces/user/user.interface';
+import { IUser } from '@interfaces/user/user.interface';
+import { AuthService } from '@services/auth/auth.service';
 import { UserEventService } from '@services/user-event/user-event.service';
 
 @Component({
@@ -9,22 +10,27 @@ import { UserEventService } from '@services/user-event/user-event.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  @Output() public menuClick = new EventEmitter();
+  @Output() public menuClick: EventEmitter<void> = new EventEmitter();
 
-  public user: User | null = null;
+  public user!: IUser;
 
   constructor(
+    private authService: AuthService,
     private userEventService: UserEventService,
     public dialog: MatDialog
   ) {}
 
   ngOnInit() {
-    if (localStorage.getItem('user')) {
-      this.user = JSON.parse(localStorage.getItem('user') || '{}');
-    }
+    this.authService.getCurrentUser().subscribe((user: IUser | null) => {
+      if (user) {
+        this.user = user;
+      }
+    });
 
-    this.userEventService.get().subscribe((user) => {
-      this.user = user;
+    this.userEventService.get().subscribe((user: IUser | null) => {
+      if (user) {
+        this.user = user;
+      }
     });
   }
 }
