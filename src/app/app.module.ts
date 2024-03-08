def81b4f28acf55f3fa48ec87b0290eb93c6cb9e
Henrique Malvar <henriquemalvar@gmail.com>
2024-03-08 14:20:07 -0300

    Refactor user section and sidenav buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './core/layout/layout.module';
-import { HomeModule } from './pages/home/home.module';
 import { SharedModule } from './shared/shared.module';
+
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,8 +21,6 @@ import { MatDividerModule } from '@angular/material/divider';
     MatSidenavModule,
     MatButtonModule,
     MatDividerModule,
-    LayoutModule,
-    HomeModule,
     LayoutModule,
     HttpClientModule,
     SharedModule,
